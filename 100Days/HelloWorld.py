169dfc6506f8d86682de3775e88101b4181e10e5
pxh <peixinhua@unisound.com>
2018-12-05 14:19:28 +0800

    add hello world code

---
+++ 100Days/HelloWorld.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+print('Hello world!')
+print('你好','世界')
+print('Hello','world',sep=',',end='!')
+print('good bye,world',end='!\n')
+
+# Prints the values to a stream, or to sys.stdout by default.
+#     Optional keyword arguments:
+#     file:  a file-like object (stream); defaults to the current sys.stdout.
+#     sep:   string inserted between values, default a space.
+#     end:   string appended after the last value, default a newline.
+#     flush: whether to forcibly flush the stream.
+
+
+
--- 100Days/venv/bin/activate.csh
+++ 100Days/venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/pxh/Documents/Python/100Days/venv"
+setenv VIRTUAL_ENV "/Users/pxh/Documents/MyPython-100-Days/100Days/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
